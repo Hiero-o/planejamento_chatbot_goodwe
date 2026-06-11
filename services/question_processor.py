84b256fb00c4769153043f3bab499da68d5b43e4
Gustavo Araujo Ramos da Silva <gustavoardss@gmail.com>
2026-06-11 01:10:23 -0300

    Implementando interface Streamlit e consultas operacionais e readme

--- services/question_processor.py
+++ services/question_processor.py
@@ -1,40 +1,114 @@
 import re
 
 from chatbot.llm import ask_model
-from service.monitoramento import (
-    get_charger
-)
+from services.monitoramento import *
+
+
 
 def process_question(
         question,
         memory
 ):
-    
-    memory.add_user_message(
-        question
-    )
 
     match = re.search(
         r"charger_(\d+)",
         question.lower()
     )
 
+    contexto = question
+
+    texto = question.lower()
+
     if match:
-        charger_id = (
-            f"charger_",
-            f"{match.group(1).zfill(2)}"
-        )
+        charger_id = f"charger_{match.group(1).zfill(2)}"
 
-        charger = get_charger(
-            charger_id
-        )
+        charger = get_charger(charger_id)
+        
+        if charger:
+            contexto = f"""
 
-        answer = ask_model(
-            memory.get_messages()
-        )
+            Dados atuais do carregador:
 
-        memory.add_assistant_message(
-            answer
-        )
+            Usuário: {charger["usuario"]}"
+            Status: {charger["status"]}"
+            Potência: {charger["potencia_kw"]} KW"
+            Corrente: {charger["corrente_a"]} A"
+            Tensão: {charger["tensao_v"]} V"
+            Energia: {charger["energia_kwh"]} KWH"
+            Tempo Restante: {charger["tempo_restante_min"]} Min"
+            Horario: {charger["horario"]}"
+            Tarifa: {charger["tarifa_kwh"]} KWH\n\n"
 
-        return answer
+            Pergunta:
+            {question}
+            """
+            
+        # Consultas agregadas
+
+    elif "potência total" in texto:
+        total = get_total_power()
+
+        contexto = f"""
+        Potência total da planta: {total} KW
+
+        Pergunta:
+        {question}
+        """
+            
+
+    elif "disponíveis" in texto:
+        disponiveis = get_available_chargers()
+
+        contexto = f"""
+        Caregadores disponíveis:
+
+        {disponiveis}
+
+        Pergunta:
+
+        {question}
+        """
+        
+
+    elif "carregadores em uso" in texto:
+        ativos = get_active_chargers()
+
+        
+        contexto = f"""
+            Carregadores em uso:
+
+            {ativos}
+
+            Pergunta:
+
+            {question}
+
+        """
+    
+
+    elif "qual a energia total usada?" in texto:
+        energia_tot = get_total_energy()
+
+        contexto= f"""
+            Total de energia usada: {energia_tot} KWH
+
+            Pergunta:
+
+            {question}
+
+        """
+
+    memory.add_user_message(
+        contexto
+    )
+
+
+    answer = ask_model(
+        memory.get_messages()
+    )
+
+    memory.add_assistant_message(
+        answer
+    )
+
+    return answer
--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,11 @@ if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
 
-for message in st.session_state.chat_history:
 
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
 
 # ------------------------------------------------------------------ #
 
-render.sidebar()
+render_sidebar()
 
 render_chat()
 
@@ -71,7 +68,7 @@ if question:
 
     st.session_state.chat_history.append(
         {
-            "role": "asistant",
+            "role": "assistant",
             "content": answer
         }
     )
